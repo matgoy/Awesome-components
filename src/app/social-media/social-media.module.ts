5a6d2bc891348d21585eb0aef19d712e3319ec3b
matgoy <gohiermathieu78@gmail.com>
2022-12-06 14:12:58 +0100

    first commit

---
+++ src/app/social-media/social-media.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SocialMediaRoutingModule } from './social-media-routing.module';
+import {PostsService} from "./services/posts.service";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SocialMediaRoutingModule
+  ],
+  providers: [
+    PostsService
+  ]
+})
+export class SocialMediaModule { }
